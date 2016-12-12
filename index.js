ffa510c8ea15fd392e74f3c9c3f1e294b72b88c5
Krista Goralczyk <krista.goralczyk@conciergeauctions.com>
2016-12-12 12:55:55 -0600

    setting up component for table

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 // will export table object for module access here
+module.exports = require('./src/app');
--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import MaterialTable from './table';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table';
 
+// needs to accept data for table header
+
+// needs to accept data for table body
+
+// # of records to show per page
+
+// sorting && filter TBD
+
+export default class ReactMuiTable extends Component {
+  render() {
+    return (
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderColumn>One Column</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableRowColumn>One row of content</TableRowColumn>
+          </TableRow>
+        </TableBody>
+      </Table>
+    );
+  }
+}
+
+// Render for local dev validation
 render(
 	<MuiThemeProvider>
-		<MaterialTable />
+		<ReactMuiTable />
 	</MuiThemeProvider>,
 	document.getElementById('app')
 );
